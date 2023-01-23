b650f4e0f485e9d496c55c32847633547cc5ec27
tom <tomcat-26@hotmail.co.uk>
2023-01-23 13:29:10 +0800

    Created concert cart styles

--- frontend/src/components/ConcertDetails.js
+++ frontend/src/components/ConcertDetails.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 export default function ConcertDetails({ concert }) {
+
   return (
     <div className='concert-details'>
         <h4 className='orchestra'>{concert.orchestra}</h4>
         <p className='location'>{concert.location}</p>
-        <p className='date'>{concert.date}</p>
-        {concert.payStatus ? 'paid' : 'unpaid'}
+        <p className='date'>{concert.date.split('-').reverse().join('/')}</p>
+        {concert.payStatus ? 
+          <div className='status paid'>paid</div> : 
+          <div className='status unpaid'>unpaid</div>
+        }
         <ul className='composers'>
           {concert.pieces.map((piece, index) => (
             <li key={index}>{piece.composer}</li>
           ))}
         </ul>
-        <ul className='pieces'>
+        <ul className='instruments'>
             {concert.instruments.map((instrument, index) => (
                 <li key={index}>{instrument}</li>
             ))}
