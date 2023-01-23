90834b4f12181eab6fbf5b0fc4166e1a2646a888
tom <tomcat-26@hotmail.co.uk>
2023-01-23 10:46:20 +0800

    created concertModel.js

---
+++ backend/models/concertModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const concertSchema = new Schema({
+    orchestra: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    payStatus: {
+        type: Boolean,
+        required: true
+    },
+    pieces: {
+        type: Array,
+        required: false
+    },
+    instruments: {
+        type: Array,
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Concert', concertSchema);
