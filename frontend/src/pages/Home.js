6e47941441a6de43e1b154ff9c07282ce299d39e
tom <tomcat-26@hotmail.co.uk>
2023-02-15 16:21:04 +0800

    Updated Home.js description of site

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,7 +4,15 @@ const Home = () => {
   return (
     <div className='home'>
         <h2>Home</h2>
-        <p>Insert description here!</p>
+        <p>Welcome to OpenGroups.</p>
+        <p>Connecting musicians to groups who require your help for upcoming concerts!</p>
+        <p>
+          Players can find orchestras, choirs, ensembles and groups of all sizes to get experience playing with and helping out. Fancy
+          trying out a specific piece? You can search for it here and see if someone has space for you!
+        </p>
+        <p>Groups can advertise their concerts, what pieces are being performed and what help they require. If they want, they are able to offer
+          financial support (e.g. travel expenses) for players who want to help out!
+        </p>
         <button>Player</button>
         <button>Group</button>
     </div>
