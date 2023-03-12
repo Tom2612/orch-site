f0de5acc9282fffabe3d12c927c330f685fd1b3d
tom <tomcat-26@hotmail.co.uk>
2023-03-12 13:24:02 +0800

    Styled homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React from 'react';
+import '../styles/home.css';
 
 const Home = () => {
   return (
-    <div className='home'>
-        <h2>Home</h2>
-        <p>Welcome to OpenGroups.</p>
-        <p>Connecting musicians to groups who require your help for upcoming concerts!</p>
-        <p>
-          Players can find orchestras, choirs, ensembles and groups of all sizes to get experience playing with and helping out. Fancy
-          trying out a specific piece? You can search for it here and see if someone has space for you!
-        </p>
-        <p>Groups can advertise their concerts, what pieces are being performed and what help they require. If they want, they are able to offer
-          financial support (e.g. travel expenses) for players who want to help out!
-        </p>
-        <button>Player</button>
-        <button>Group</button>
+    <div className='container'>
+        {/* <h1>Welcome!</h1> */}
+        <div className='open-message'>
+          <h3>Welcome to OpenGroups.</h3>
+          <p>Connecting musicians to groups who require your help for upcoming concerts!</p>
+        </div>
+        <div className='blocks'>
+          <div className='block'>
+            <h4><span className='material-symbols-outlined'>person</span>Player login</h4>
+            <p>Players can find orchestras, choirs, ensembles and groups of all sizes to get experience playing with and helping out. Fancy
+              trying out a specific piece? You can search for it here and see if someone has space for you!</p>
+            <button className='btn update-btn'>Player</button>
+          </div>
+          <div className='block'>
+            <h4><span className='material-symbols-outlined'>group</span>Group login</h4>
+            <p>Groups can advertise their concerts, what pieces are being performed and what help they require. If they want, they are able to offer
+            financial support (e.g. travel expenses) for players who want to help out!</p>
+            <button className='btn update-btn'>Group</button>
+          </div>
+        </div>
     </div>
   )
 }
